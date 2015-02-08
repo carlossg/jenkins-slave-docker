2272c012f7aee33672562216bbe418a42bb2142e
Carlos Sanchez <carlos@apache.org>
2015-02-08 20:33:47 +0100

    Allow both JENKINS_JNLP_URL and JENKINS_SECRET

--- jenkins-slave.sh
+++ jenkins-slave.sh
@@ -6,14 +6,16 @@ if [[ $# -lt 1 ]] || [[ "$1" == "-"* ]]; then
   # jenkins slave
   JAR=`ls -1 /usr/share/jenkins/remoting-*.jar | tail -n 1`
 
+  PARAMS=""
+
   # if -jnlpUrl is not provided try env var
   if [[ "$@" != *"-jnlpUrl "* ]] && [ ! -z "$JENKINS_JNLP_URL" ]; then
-    PARAMS="-jnlpUrl $JENKINS_JNLP_URL"
+    PARAMS="$PARAMS -jnlpUrl $JENKINS_JNLP_URL"
   fi
 
   # if -secret is not provided try env var
   if [[ "$@" != *"-secret "* ]] && [ ! -z "$JENKINS_SECRET" ]; then
-    PARAMS="-secret $JENKINS_SECRET"
+    PARAMS="$PARAMS -secret $JENKINS_SECRET"
   fi
 
   echo Running java $JAVA_OPTS -jar $JAR $PARAMS "$@"
