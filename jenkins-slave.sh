4b0d0f7a8f36cc65acb47c31507fe52932f8577c
Carlos Sanchez <carlos@apache.org>
2015-02-14 15:25:55 +0100

    Execute hudson.remoting.jnlp.Main to allow setting -tunnel

--- jenkins-slave.sh
+++ jenkins-slave.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
 
-# if `docker run` first argument start with `-` the user is passing jenkins launcher arguments
-if [[ $# -lt 1 ]] || [[ "$1" == "-"* ]]; then
+# Usage jenkins-slave.sh [options] -url http://jenkins SECRET SLAVE_NAME
+
+# if `docker run` has 2 or more arguments the user is passing jenkins launcher arguments
+if [[ $# -gt 1 ]]; then
 
   # jenkins slave
   JAR=`ls -1 /usr/share/jenkins/remoting-*.jar | tail -n 1`
 
   PARAMS=""
 
-  # if -jnlpUrl is not provided try env var
-  if [[ "$@" != *"-jnlpUrl "* ]] && [ ! -z "$JENKINS_JNLP_URL" ]; then
-    PARAMS="$PARAMS -jnlpUrl $JENKINS_JNLP_URL"
+  # if -url is not provided try env var
+  if [[ "$@" != *"-url "* ]] && [ ! -z "$JENKINS_URL" ]; then
+    PARAMS="$PARAMS -url $JENKINS_URL"
   fi
 
-  # if -secret is not provided try env var
-  if [[ "$@" != *"-secret "* ]] && [ ! -z "$JENKINS_SECRET" ]; then
-    PARAMS="$PARAMS -secret $JENKINS_SECRET"
+  # if -tunnel is not provided try env var
+  if [[ "$@" != *"-tunnel "* ]] && [ ! -z "$JENKINS_TUNNEL" ]; then
+    PARAMS="$PARAMS -tunnel $JENKINS_TUNNEL"
   fi
 
   echo Running java $JAVA_OPTS -jar $JAR $PARAMS "$@"
-  exec java $JAVA_OPTS -jar $JAR $PARAMS "$@"
+  exec java $JAVA_OPTS -cp $JAR hudson.remoting.jnlp.Main -headless $PARAMS "$@"
 fi
 
 # As argument is not jenkins, assume user want to run his own process, for sample a `bash` shell to explore this image
